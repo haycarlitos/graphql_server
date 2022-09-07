c211f56b3079e20ed19ae3cdb5e7959f9b70ca44
haycarlitos <carlos.castilloperera@gmail.com>
2022-09-07 16:54:11 -0500

    add relay mutations

--- hackernews/schema.py
+++ hackernews/schema.py
@@ -2,11 +2,21 @@ import graphene
 import graphql_jwt
 import links.schema
 from hackernews.users import schema 
+import links.schema_relay
 
-class Query(schema.Query, links.schema.Query, graphene.ObjectType):
+class Query(
+    schema.Query,
+    links.schema.Query,
+    links.schema_relay.RelayQuery,
+    graphene.ObjectType,
+    ):
     pass
 
-class Mutation(schema.Mutation, links.schema.Mutation, graphene.ObjectType):
+class Mutation(schema.Mutation,
+            links.schema.Mutation,
+            links.schema_relay.RelayMutation,
+            graphene.ObjectType):
+            
     token_auth = graphql_jwt.ObtainJSONWebToken.Field()
     verify_token = graphql_jwt.Verify.Field()
     refresh_token = graphql_jwt.Refresh.Field()
