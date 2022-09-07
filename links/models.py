37a4672ee2d90fe89ea255a9f28bbd5ecd3410fb
haycarlitos <carlos.castilloperera@gmail.com>
2022-09-07 12:39:16 -0500

    adding votes: model and schema (with migrations)

--- links/models.py
+++ links/models.py
@@ -1,3 +1,4 @@
+from tkinter import CASCADE
 from django.db import models
 from django.conf import settings
 
@@ -6,3 +7,8 @@ class Link(models.Model):
     url = models.URLField()
     description = models.TextField(blank=True)
     posted_by = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, on_delete=models.CASCADE)
+
+
+class Vote(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    link = models.ForeignKey('links.Link', related_name='votes', on_delete=models.CASCADE)
