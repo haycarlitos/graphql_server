d52f6d5b40ca46eafe4a9e6bae708ae1fcb60dde
haycarlitos <carlos.castilloperera@gmail.com>
2022-09-07 11:53:10 -0500

    initial commit

---
+++ hackernews/schema.py
@@ -0,0 +1,17 @@
+import graphene
+import graphql_jwt
+import links.schema
+from hackernews.users import schema 
+
+class Query(schema.Query, links.schema.Query, graphene.ObjectType):
+    pass
+
+class Mutation(schema.Mutation, links.schema.Mutation, graphene.ObjectType):
+    token_auth = graphql_jwt.ObtainJSONWebToken.Field()
+    verify_token = graphql_jwt.Verify.Field()
+    refresh_token = graphql_jwt.Refresh.Field()
+
+
+
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
